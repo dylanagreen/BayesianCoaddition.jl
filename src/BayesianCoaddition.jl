7d0daf775fc23318f17badb1523bb215b0193cdf
dylanagreen <dylanag@uci.edu>
2025-08-01 12:09:10 -0700

    Initial commit of code including exposure accumulation.

--- src/BayesianCoaddition.jl
+++ src/BayesianCoaddition.jl
@@ -1,5 +1,35 @@
 module BayesianCoaddition
+using LinearAlgebra
 
-# Write your package code here.
+export CoaddLikelihood, accumulate_exposure, get_bayes_coadd, construct_likelihood
+# export approximate_sigma_f
+
+# include("approximation.jl")
+
+mutable struct CoaddLikelihood
+    A
+    phi
+    sigma_f::Float64
+end
+
+# Should be used to generate a coadd likelihood struct from a single exposure.
+function construct_likelihood(flux, ivar, psf_mat)
+    A = psf_mat * diagm(ivar) * psf_mat'
+    phi = psf_mat * (flux .* ivar)
+    return CoaddLikelihood(A, phi, 1)
+end
+
+function get_bayes_coadd(likelihood::CoaddLikelihood)
+    sigma_f_matrix = diagm(ones(length(likelihood.phi)) .* (likelihood.sigma_f^-2))
+    A_prior = sparse(likelihood.A .+ sigma_f_matrix)
+    return A_prior \ likelihood.phi
+end
+
+function accumulate_exposure(likelihood::CoaddLikelihood, flux, ivar, psf_mat)
+    likelihood.A += psf_mat * diagm(ivar) * psf_mat'
+    likelihood.phi += psf_mat * (flux .* ivar)
+end
+
+
 
 end
---
+++ src/approximation.jl
@@ -0,0 +1,76 @@
+# Named this S because it actually comes from the "scaling gaussian"
+# that results when multiplying two gaussians (the prior and the likelihood)
+function log_S(stats::SufficientStats, sigma_f)
+  sigma_f_matrix = diagm(ones(length(stats.phi)) .* (sigma_f^-2))
+  A_prior = sparse(stats.A .+ sigma_f_matrix)
+  return -logdet(A_prior) * 0.5 - length(stats.phi) * log(sigma_f) + 0.5 * stats.phi' * (A_prior \ stats.phi)
+end
+
+# function dlog_S(A, phi, sigma_f)
+#   sigma_f_matrix = diagm(ones(length(phi)) .* (sigma_f^-2))
+#   A_prior = Symmetric(A .+ sigma_f_matrix)
+#   f_bar = A_prior \ phi
+#   return (sigma_f^-2) * (tr(inv(A_prior)) + (f_bar' * f_bar)) - length(phi)
+# end
+
+function fit_and_find_vertex(sigma_vals, S_vals)
+    # Finds the rough estimate of the vertex from the coarse evaluations we did
+    idx = argmax(S_vals)
+
+    # We will fit to the two points either side of the
+    # estimated vertex, since the function is approximately
+    # quadratic there (plus a logarithmic component to fit for the log
+    # dependence in the true function)
+    fit_range = idx - 2 : idx + 2
+
+    if fit_range[1] < 1
+        fit_range = fit_range .- fit_range[1] .+ 1
+    elseif fit_range[end] > length(S_vals)
+        fit_range = fit_range .- (fit_range[end] - length(S_vals))
+    end
+
+    fit_vals = sigma_vals[fit_range]
+    X_log_fit = [fit_vals .^ 2 fit_vals log.(fit_vals) ones(length(fit_vals))]
+    coeffs_log = X_log_fit \ S_vals[fit_range]
+
+    # Analytic maximum found taking derivatives of the fit function and the quadratic formula
+    vertex_log = (-coeffs_log[2] - sqrt(coeffs_log[2]^2 - 8 * coeffs_log[1] * coeffs_log[3]))/ (4 * coeffs_log[1])
+    return vertex_log
+end
+
+
+function approximate_sigma_f(stas::SufficientStats; n_iter::Int=2, n_points::Int=15)
+  # Initial scan
+  sigma_vals_full = Vector(range(0.05, 1, n_points))
+  S_vals = Vector{Float64}()
+
+  for sig in sigma_vals_full
+      S = log_S(stats, sig, false)
+      append!(S_vals, S)
+  end
+
+  vertex = fit_and_find_vertex(sigma_vals_full, S_vals)
+  for i in range(0, n_iter)
+      insert_idx_log = searchsorted(sigma_vals_full, vertex)
+
+      # Inserts the estimated vertex and its log_s value
+      # into the arrays for the next pass
+      splice!(sigma_vals_full, insert_idx_log, vertex)
+      splice!(S_vals, insert_idx_log, log_S(stats, vertex))
+
+      vertex = fit_and_find_vertex(sigma_vals_full, S_vals)
+  end
+
+  # return vertex
+  # Update the sufficient stats.
+  stats.sigma_f = vertex
+end
+
+# function get_bayes_coadd(A, phi, sigma_f)
+#   sigma_f_matrix = diagm(ones(length(phi)) .* (sigma_f^-2))
+#   A_prior = sparse(A .+ sigma_f_matrix)
+#   return A_prior \ phi
+# end
+
+
+
